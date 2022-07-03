a2a582528aab94cebadf9d0d0376784ef9eae537
Berat Özmen <beratzmen@gmail.com>
2022-07-03 15:57:22 +0300

    Distributed cache added

---
+++ IDistributedCacheRedisInMvc/Controllers/ProductsController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+
+namespace IDistributedCacheRedisInMvc.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private IDistributedCache _distributedCache;
+
+        public ProductsController(IDistributedCache distributedCache)
+        {
+            _distributedCache = distributedCache;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var cacheEntryOptions = new DistributedCacheEntryOptions();
+
+            //Cache üzerinde 5 dakika durması için.
+            cacheEntryOptions.AbsoluteExpiration = DateTimeOffset.Now.AddMinutes(5);
+
+            //_distributedCache.SetString("keysss", "valuesss", cacheEntryOptions);
+            await _distributedCache.SetStringAsync("keysss", "valuesss", cacheEntryOptions);
+
+            return View();
+        }
+
+        public IActionResult Show()
+        {
+            //keysss keyine ait value görüntülenir.
+            string name = _distributedCache.GetString("keysss");
+
+            ViewBag.Name = name;
+
+            return View();
+        }
+
+        public IActionResult Delete()
+        {
+            //Keysss keyindeki value silinir.
+            _distributedCache.Remove("keysss");
+
+            return View();
+        }
+    }
+}
