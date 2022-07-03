cecca3e8dcc7cf46e11ebb43178305d2b07b9717
Berat Özmen <beratzmen@gmail.com>
2022-07-03 16:21:48 +0300

    Json object and byteArray get, set methods

--- IDistributedCacheRedisInMvc/Controllers/ProductsController.cs
+++ IDistributedCacheRedisInMvc/Controllers/ProductsController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Caching.Distributed;
+using System.Text;
+using System.Text.Json;
 
 namespace IDistributedCacheRedisInMvc.Controllers
 {
@@ -19,18 +21,46 @@ namespace IDistributedCacheRedisInMvc.Controllers
             //Cache üzerinde 5 dakika durması için.
             cacheEntryOptions.AbsoluteExpiration = DateTimeOffset.Now.AddMinutes(5);
 
-            //_distributedCache.SetString("keysss", "valuesss", cacheEntryOptions);
-            await _distributedCache.SetStringAsync("keysss", "valuesss", cacheEntryOptions);
+            ////_distributedCache.SetString("keysss", "valuesss", cacheEntryOptions);
+            //await _distributedCache.SetStringAsync("keysss", "valuesss", cacheEntryOptions);
+
+            var product = new Models.Product
+            {
+                Id = 2,
+                Name = "Kalem2",
+                Price = 5
+            };
+
+            //Cache üzerine json olarak da kayıt edilebilir.
+            string jsonProduct = JsonSerializer.Serialize(product);
+
+            //Byte array olarak cache kayıt etmek için.
+            var byteProduct = Encoding.UTF8.GetBytes(jsonProduct);
+
+            //Json olarak kayıt etmek için
+            //await _distributedCache.SetStringAsync("product:2", jsonProduct, cacheEntryOptions);
+
+            //Byte array olarak kayıt etmek için.
+            await _distributedCache.SetAsync("product:2", byteProduct, cacheEntryOptions);
 
             return View();
         }
 
-        public IActionResult Show()
+        public async Task<IActionResult> Show()
         {
-            //keysss keyine ait value görüntülenir.
-            string name = _distributedCache.GetString("keysss");
+            ////keysss keyine ait value görüntülenir.
+            //string name = _distributedCache.GetString("keysss");
 
-            ViewBag.Name = name;
+            var jsonProduct = await _distributedCache.GetStringAsync("product:2");
+
+            var product = JsonSerializer.Deserialize<Models.Product>(jsonProduct);
+
+            //Byte array'in cache üzerinden çekilmesi için.
+            var byteProduct = _distributedCache.Get("product:2");
+            var jsonProductToByteArray = Encoding.UTF8.GetString(byteProduct);
+
+            //ViewBag.Name = name;
+            ViewBag.Product = jsonProductToByteArray;
 
             return View();
         }
